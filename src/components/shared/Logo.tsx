ab09e1266cec8fac688ea34994c36468074d5c51
Deepak Bagada <dbagada910@gmail.com>
2025-09-01 05:38:56 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Logo>
    
    with the t

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -1,11 +1,10 @@
+import Image from 'next/image';
 import Link from 'next/link';
-import { Wind } from 'lucide-react';
 
 export default function Logo() {
   return (
     <Link href="/" className="flex items-center space-x-2">
-      <Wind className="h-6 w-6 text-primary" />
-      <span className="text-xl font-bold font-headline">DreamWeave Digital</span>
+      <Image src="/logo.jpeg" alt="DreamWeave Digital Logo" width={150} height={40} />
     </Link>
   );
 }
