f8141f3765c45ed7f75785d39da4eebe787910a6
Deepak Bagada <dbagada910@gmail.com>
2025-09-01 05:35:25 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,15 +35,9 @@ export default function RootLayout({
           'min-h-screen bg-background font-body antialiased flex flex-col'
         )}
       >
-        <div className="relative z-0 overflow-x-clip flex flex-col flex-grow">
-          <div className="absolute top-0 -left-64 w-96 h-96 bg-secondary rounded-full opacity-30 animate-pulse" />
-          <div className="absolute -bottom-64 -right-32 w-96 h-96 bg-secondary rounded-full opacity-30 animate-pulse animation-delay-3000" />
-          <div className="relative z-10 flex flex-col flex-grow">
-            <Header />
-            <main className="flex-grow animate-in fade-in duration-500">{children}</main>
-            <Footer />
-          </div>
-        </div>
+        <Header />
+        <main className="flex-grow animate-in fade-in duration-500">{children}</main>
+        <Footer />
         <Toaster />
       </body>
     </html>
